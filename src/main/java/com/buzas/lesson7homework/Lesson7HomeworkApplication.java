fb5fc3549f283550d77c40af1d1c914615f8446f
Vito <vitavtasbuzas@gmail.com>
2023-02-07 19:46:07 +0300

    Initial commit

---
+++ src/main/java/com/buzas/lesson7homework/Lesson7HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.buzas.lesson7homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson7HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson7HomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buzas/lesson7homework/Lesson7HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzas.lesson7homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson7HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
